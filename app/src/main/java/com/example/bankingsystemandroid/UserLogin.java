55e9c039007c08037f010abb872123ed1f499e18
Richu <richujain1995@gmail.com>
2019-07-27 23:49:04 -0400

    [Richu] Firebase Login Code added.

--- app/src/main/java/com/example/bankingsystemandroid/UserLogin.java
+++ app/src/main/java/com/example/bankingsystemandroid/UserLogin.java
@@ -1,14 +1,20 @@
 package com.example.bankingsystemandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -37,9 +43,28 @@ public class UserLogin extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                boolean flag = validation();
                 if (validation() == true){
+                    String userUsername = username.getText().toString().trim();
+                    String userPassword = password.getText().toString().trim();
+                    mAuth.signInWithEmailAndPassword(userUsername, userPassword)
+                            .addOnCompleteListener(UserLogin.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        FirebaseUser user = mAuth.getCurrentUser();
 
+                                        //updateUI(user);
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Toast.makeText(UserLogin.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+                                        //updateUI(null);
+                                    }
+
+                                    // ...
+                                }
+                            });
                 }
             }
         });
