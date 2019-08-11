3caf81086dc3ab877584e1f1bd0376500a130504
Abhishek <abhishek.madepogu@gmail.com>
2019-08-11 19:41:56 -0400

    [Abhishek]Added TransactionClass

---
+++ app/src/main/java/com/example/bankingsystemandroid/Transaction.java
@@ -0,0 +1,9 @@
+package com.example.bankingsystemandroid;
+
+import java.util.Date;
+
+public class Transaction {
+    Date transactionDate;
+    int Amount;
+    String typeofTrnasaction;
+}
--- app/src/main/java/com/example/bankingsystemandroid/TransactionAdapter.java
+++ app/src/main/java/com/example/bankingsystemandroid/TransactionAdapter.java
@@ -1,4 +1,59 @@
 package com.example.bankingsystemandroid;
 
-public class TransactionAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.ViewHolder> {
+
+    private List<Transaction> transactions;
+    private Context context;
+
+    public TransactionAdapter(List<Transaction> transactions, Context context) {
+        this.transactions = transactions;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.transaction_cell,parent,false);
+        return new ViewHolder(v);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+       Transaction transaction=transactions.get(position);
+        holder.tvFname.setText(transaction.);
+        holder.tvLname.setText(student.getLastName()) ;
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return transactions.size();
+    }
+
+    public  class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView tvDate;
+        public TextView tvAmount;
+        public TextView tvTransactionType;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvDate=(TextView)itemView.findViewById(R.id.tvDate);
+            tvAmount=(TextView)itemView.findViewById(R.id.tvAmount);
+            tvTransactionType=itemView.findViewById(R.id.tvTransactionType);
+        }
+    }
 }
+
