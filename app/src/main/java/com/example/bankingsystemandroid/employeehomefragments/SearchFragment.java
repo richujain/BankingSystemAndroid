8a759be53c7432d0b6ec1c9aba2709d7f84af996
Richu <richujain1995@gmail.com>
2019-08-12 20:22:46 -0400

    [Richu] Error Resolved. onClick implemented for the search image.

--- app/src/main/java/com/example/bankingsystemandroid/employeehomefragments/SearchFragment.java
+++ app/src/main/java/com/example/bankingsystemandroid/employeehomefragments/SearchFragment.java
@@ -5,9 +5,11 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -20,8 +22,13 @@ import com.example.bankingsystemandroid.R;
 public class SearchFragment extends Fragment {
 
     private EditText accountNumber;
-    private ImageView search;
+    private Button search;
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 
     @Nullable
     @Override
@@ -29,9 +36,11 @@ public class SearchFragment extends Fragment {
         View layout = inflater.inflate(R.layout.fragment_search, container, false);
         accountNumber = layout.findViewById(R.id.accountNumber);
         search = layout.findViewById(R.id.searchImage);
+        Log.v("blah inside","blah inside");
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.v("blah","blah");
                 searchCustomer();
             }
         });
@@ -40,7 +49,9 @@ public class SearchFragment extends Fragment {
         //with the fragment you want to inflate
         //like if the class is HomeFragment it should have R.layout.home_fragment
         //if it is DashboardFragment it should have R.layout.fragment_dashboard
-        return inflater.inflate(R.layout.fragment_search, null);
+        //Error resolved https://stackoverflow.com/questions/23490880/button-onclick-inside-fragment-is-not-working
+        //return inflater.inflate(R.layout.fragment_search, null);
+        return layout;
     }
 
     private void searchCustomer(){
