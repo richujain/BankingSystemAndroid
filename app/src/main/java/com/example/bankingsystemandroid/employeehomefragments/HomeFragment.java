2929f0ccf5366a9c84d4cd3941a7aaaa401f1e09
Richu <richujain1995@gmail.com>
2019-08-14 09:00:22 -0400

    [Richu] Data adding to Firebase. Need some changes though.

--- app/src/main/java/com/example/bankingsystemandroid/employeehomefragments/HomeFragment.java
+++ app/src/main/java/com/example/bankingsystemandroid/employeehomefragments/HomeFragment.java
@@ -1,13 +1,20 @@
 package com.example.bankingsystemandroid.employeehomefragments;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,13 +27,17 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
+import java.util.Random;
 
 public class HomeFragment extends Fragment {
 
     EditText customerBirthDate,customerName,customerAddress,customerContactNumber,customerEmailId,customerPhotoAddressProofId;
+    EditText customerAccountBalance,customerBankBranch;
     Calendar myCalendar;
     DatePickerDialog.OnDateSetListener date;
     Button addUserButton;
+    RadioGroup radioGroup;
+    private RadioButton radioButton;
 
     @Nullable
     @Override
@@ -44,6 +55,14 @@ public class HomeFragment extends Fragment {
         customerEmailId = layout.findViewById(R.id.emailId);
         customerPhotoAddressProofId = layout.findViewById(R.id.photoAddressIdProof);
         addUserButton = layout.findViewById(R.id.addUserButton);
+        radioGroup = layout.findViewById(R.id.radioGroup);
+        customerAccountBalance = layout.findViewById(R.id.accountBalance);
+        customerBankBranch = layout.findViewById(R.id.bankBranch);
+        //first 2131296358
+        //second 2131296446
+        int selectedId = radioGroup.getCheckedRadioButtonId();
+        // find the radiobutton by returned id
+        radioButton = layout.findViewById(selectedId);
         addUserButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -92,8 +111,49 @@ public class HomeFragment extends Fragment {
         Boolean flag = validateDetails();
         if(flag){
             FirebaseDatabase database = FirebaseDatabase.getInstance();
-            DatabaseReference myRef = database.getReference("message");
+            DatabaseReference myRef = database.getReference("customers");
+            Random rand = new Random();
+            int accountNumber = rand.nextInt(9000000) + 1000000;
+            Log.v("accountnumber",""+accountNumber);
+            //Fetching and storing user details to a local variable
+            String name = customerName.getText().toString();
+            String birthDate = customerBirthDate.getText().toString();
+            String address = customerAddress.getText().toString();
+            String contactNumber = customerContactNumber.getText().toString();
+            String photoAddressIdProof = customerPhotoAddressProofId.getText().toString();
+            String accountType = radioButton.getText().toString();
+            String accountBalance = customerAccountBalance.getText().toString();
+            String bankBranch = customerBankBranch.getText().toString();
+            //Storing Data to Firebase
+            myRef.child(""+accountNumber).child("name").setValue(name);
+            myRef.child(""+accountNumber).child("birthdate").setValue(birthDate);
+            myRef.child(""+accountNumber).child("address").setValue(address);
+            myRef.child(""+accountNumber).child("contactnumber").setValue(contactNumber);
+            myRef.child(""+accountNumber).child("photoaddressproofid").setValue(photoAddressIdProof);
+            myRef = database.getReference("bank");
+            myRef.child(accountType).child(""+accountNumber).child("accountbalance").setValue(accountBalance);
+            myRef.child(accountType).child(""+accountNumber).child("bankbranch").setValue(bankBranch);
+            myRef.child(accountType).child(""+accountNumber).child("accountnumber").setValue(accountNumber);
+            showAlert("Success","Customer Added. \n Account Number is "+accountNumber,getContext());
         }
+    }
+    private void showAlert(String title,String message, Context context){
+        AlertDialog.Builder alertDialog2 = new AlertDialog.Builder(
+                context);
+
+        alertDialog2.setTitle(title);
+
+        alertDialog2.setMessage(message);
+
+
+        alertDialog2.setPositiveButton("OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Write your code here to execute after dialog
+                        dialog.cancel();
+                    }
+                });
+        alertDialog2.show();
     }
     private Boolean validateDetails(){
         String thisFieldCannotBeEmpty = "This Field Cannot Be Empty";
@@ -121,6 +181,12 @@ public class HomeFragment extends Fragment {
         }
         else if(customerPhotoAddressProofId.getText().toString().trim().length() == 0){
             customerPhotoAddressProofId.setError(thisFieldCannotBeEmpty);
+            flag = false;
+        }else if(customerAccountBalance.getText().toString().trim().length() == 0){
+            customerAccountBalance.setError(thisFieldCannotBeEmpty);
+            flag = false;
+        }else if(customerBankBranch.getText().toString().trim().length() == 0){
+            customerBankBranch.setError(thisFieldCannotBeEmpty);
             flag = false;
         }
         return flag;
