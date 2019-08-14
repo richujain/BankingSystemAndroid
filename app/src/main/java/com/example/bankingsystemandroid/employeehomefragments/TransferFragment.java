2439ad39aab1e4183a0bedb34c368e31c0269b7e
Richu <richujain1995@gmail.com>
2019-08-14 13:19:43 -0400

    [Richu] Desinging Transfer Page.

--- app/src/main/java/com/example/bankingsystemandroid/employeehomefragments/TransferFragment.java
+++ app/src/main/java/com/example/bankingsystemandroid/employeehomefragments/TransferFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -11,6 +13,10 @@ import androidx.fragment.app.Fragment;
 import com.example.bankingsystemandroid.R;
 
 public class TransferFragment extends Fragment {
+    Button depositTitle,withdrawlTitle,transferTitle;
+    RelativeLayout depositLayout,withdrawlLayout,transferLayout;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -18,6 +24,49 @@ public class TransferFragment extends Fragment {
         //with the fragment you want to inflate
         //like if the class is HomeFragment it should have R.layout.home_fragment
         //if it is DashboardFragment it should have R.layout.fragment_dashboard
-        return inflater.inflate(R.layout.fragment_transaction, null);
+        //return inflater.inflate(R.layout.fragment_transaction, null);
+
+        View layout = inflater.inflate(R.layout.fragment_transaction, container, false);
+        init(layout);
+
+
+
+
+        return layout;
+    }
+    private void init(View view){
+        depositTitle = view.findViewById(R.id.depositTitle);
+        withdrawlTitle = view.findViewById(R.id.withdrawlTitle);
+        transferTitle = view.findViewById(R.id.transferTitle);
+        depositLayout = view.findViewById(R.id.depositLayout);
+        withdrawlLayout = view.findViewById(R.id.withdrawlLayout);
+        transferLayout = view.findViewById(R.id.transferLayout);
+        setOnClick();
+    }
+    private void setOnClick(){
+        depositTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                depositLayout.setVisibility(View.VISIBLE);
+                withdrawlLayout.setVisibility(View.INVISIBLE);
+                transferLayout.setVisibility(View.INVISIBLE);
+            }
+        });
+        withdrawlTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                depositLayout.setVisibility(View.INVISIBLE);
+                withdrawlLayout.setVisibility(View.VISIBLE);
+                transferLayout.setVisibility(View.INVISIBLE);
+            }
+        });
+        transferTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                depositLayout.setVisibility(View.INVISIBLE);
+                withdrawlLayout.setVisibility(View.INVISIBLE);
+                transferLayout.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
