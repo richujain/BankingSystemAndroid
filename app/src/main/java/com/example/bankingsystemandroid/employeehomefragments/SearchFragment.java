d03e63b3851addb3c9819a5af91169676e957caf
Richu <richujain1995@gmail.com>
2019-08-14 10:25:11 -0400

    [Richu] Trying to fetch data from firebase by passing account number entered by user in the search fragment.

--- app/src/main/java/com/example/bankingsystemandroid/employeehomefragments/SearchFragment.java
+++ app/src/main/java/com/example/bankingsystemandroid/employeehomefragments/SearchFragment.java
@@ -12,28 +12,44 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.bankingsystemandroid.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
 
 public class SearchFragment extends Fragment {
 
     private EditText accountNumber;
     private Button search;
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
+    TextView customerName,customerAddress,customerBirthDate,customerEmailId,customerContactNumber,customerPhotoAddressIdProof;
+    ScrollView scrollView;
+    TextView customerAccountNumber,customerAccountBalance,customerBankBranch;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View layout = inflater.inflate(R.layout.fragment_search, container, false);
+        customerPhotoAddressIdProof = layout.findViewById(R.id.customerPhotoAddressIdProof);
+        customerAddress = layout.findViewById(R.id.customerAddress);
+        customerBirthDate = layout.findViewById(R.id.customerBirthDate);
+        customerContactNumber = layout.findViewById(R.id.customerContactNumber);
+        customerEmailId = layout.findViewById(R.id.customerEmailId);
+        customerName = layout.findViewById(R.id.customerName);
+        customerAccountBalance = layout.findViewById(R.id.customerAccountBalance);
+        customerAccountNumber = layout.findViewById(R.id.customerAccountNumber);
+        customerBankBranch = layout.findViewById(R.id.customerBankBranch);
+        scrollView = layout.findViewById(R.id.scrollViewSearch);
+        scrollView.setVisibility(View.INVISIBLE);
         accountNumber = layout.findViewById(R.id.accountNumber);
         search = layout.findViewById(R.id.searchImage);
         search.setOnClickListener(new View.OnClickListener() {
@@ -55,10 +71,47 @@ public class SearchFragment extends Fragment {
     private void searchCustomer(){
         String userAccountNumber = accountNumber.getText().toString().trim();
         if(userAccountNumber.isEmpty()){
-            showAlert("Please Enter A Valid Account Number",this.getContext());
+            showAlert("Enter A Valid Account Number",this.getContext());
         }
         else{
-            
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference myRef = database.getReference("customers");
+            // Read from the database
+            myRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    // This method is called once with the initial value and again
+                    // whenever data at this location is updated.
+                    String accountNumberToSearch = accountNumber.getText().toString().trim();
+                    String name = "";
+                    name = (String) dataSnapshot.child(accountNumberToSearch).child("name").getValue();
+                    if(name.length() == 0){
+                        showAlert("No customer record found associated with the account number "+ accountNumberToSearch,getContext());
+                    }
+                    else{
+                        customerName.setText(name);
+                        String dummyVariable = (String) dataSnapshot.child(accountNumberToSearch).child("address").getValue();
+                        customerAddress.setText(dummyVariable);
+                        dummyVariable = (String) dataSnapshot.child(accountNumberToSearch).child("birthdate").getValue();
+                        customerBirthDate.setText(dummyVariable);
+                        dummyVariable = (String) dataSnapshot.child(accountNumberToSearch).child("contactnumber").getValue();
+                        customerContactNumber.setText(dummyVariable);
+                        dummyVariable = (String) dataSnapshot.child(accountNumberToSearch).child("emailid").getValue();
+                        customerEmailId.setText(dummyVariable);
+                        dummyVariable = (String) dataSnapshot.child(accountNumberToSearch).child("photoaddressproofid").getValue();
+                        customerPhotoAddressIdProof.setText(dummyVariable);
+                        scrollView.setVisibility(View.VISIBLE);
+                    }
+
+                    //Log.d(TAG, "Value is: " + value);
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    // Failed to read value
+                    Log.w(TAG, "Failed to read value.", error.toException());
+                }
+            });
         }
     }
     private void showAlert(String message, Context context){
