b706266e71aa98bed663aa209324a090e6b8bf2f
Abhishek <abhishek.madepogu@gmail.com>
2019-08-16 19:27:01 -0400

    [Abhishek]Added Email Message and Call

--- app/src/main/java/com/example/bankingsystemandroid/ContactUs.java
+++ app/src/main/java/com/example/bankingsystemandroid/ContactUs.java
@@ -2,13 +2,40 @@ package com.example.bankingsystemandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ContactUs extends AppCompatActivity {
-
+    Button btnCall,btnMessage,btnEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
+        btnCall=findViewById(R.id.btnCall);
+        btnEmail=findViewById(R.id.btnEmail);
+        btnMessage=findViewById(R.id.btnSms);
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ContactUs.this, "Calling 1234567890", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent smsIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:" + 1234567890));
+                startActivity(smsIntent);
+            }
+        });
+        btnEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ContactUs.this,EmailActivity.class));
+            }
+        });
     }
 }
